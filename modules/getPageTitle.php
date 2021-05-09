beb7c4665d5183449ed63e3ca04a0f3f5909be68
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2021-05-09 21:05:12 +0100

    modules and tos

---
+++ modules/getPageTitle.php
@@ -0,0 +1,16 @@
+<?php
+    function page_title($url) {
+        $fp = file_get_contents($url);
+        if (!$fp) 
+            return null;
+
+        $res = preg_match("/<title>(.*)<\/title>/siU", $fp, $title_matches);
+        if (!$res) 
+            return null; 
+
+        // Clean up title: remove EOL's and excessive whitespace.
+        $title = preg_replace('/\s+/', ' ', $title_matches[1]);
+        $title = trim($title);
+        return $title;
+    }
+?>
